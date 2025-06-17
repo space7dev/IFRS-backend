44f285e33566c832bc6087cc817b5db2799c6f79
Mykola <apollot7dev@gmail.com>
2025-06-17 14:14:21 -0400

    fix backend

--- deployment/gunicorn/gunicorn_dev.py
+++
@@ -1,8 +0,0 @@
-# BINDING SOCKET HERE
-bind = "unix:/run/chatgpt-gunicorn.sock"
-
-# 'NUMBER_OF_WORKERS_IN_INT' usually 2*cpu_core + 1
-workers = 1  # assuming single core cpu
-
-# USER
-user = "django"
